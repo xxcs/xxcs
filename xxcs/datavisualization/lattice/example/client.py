4624565d05544b587e21365d9e8b3eec744b0cd5
Snow Si(司龙湖) <silonghu@inspur.com>
2018-08-21 19:24:06 +0800

    JAVA信号灯

--- xxcs/datavisualization/lattice/example/client.py
+++ xxcs/datavisualization/lattice/example/client.py
@@ -1,19 +1,24 @@
-#!/usr/bin/python3.6
-# coding: utf-8
-
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 import os
-import pandas as pd
-import pandas as pd
-import seaborn as sns
-import seaborn as sns
-import matplotlib.pyplot as plt
-import matplotlib.pyplot as plt
 
-pwd = os.getcwd()
-csv_file = pwd + os.sep + "ucdavis.csv"
-print(csv_file)
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+csv_file = dir_path + os.sep + 'ucdavis.csv'
+
 students = pd.read_csv(csv_file)
 g = sns.FacetGrid(students, hue="gender", palette="Set1", height=6)
 g.map(plt.scatter, "gpa", "computer", s=250, linewidth=0.65, edgecolor="white")
 
 g.add_legend()
+
+
+
+
+
+
+
+
+
+
