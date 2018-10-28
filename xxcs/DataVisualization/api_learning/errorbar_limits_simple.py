05141a79a90914ad46a0965558d3acc310ffbe2d
xxcs <2804536059@qq.com>
2018-10-28 17:37:31 +0800

    数据可视化

---
+++ xxcs/DataVisualization/api_learning/errorbar_limits_simple.py
@@ -0,0 +1,53 @@
+# _^^_coding: utf-8 _^^_
+
+
+"""
+Errorbar Limits
+误差条界限
+Illustration of upper and lower limit symbols on errorbars
+误差条上下限符号的插图
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+fig = plt.figure(0)
+# x: [0. 1. 2. 3. 4. 5. 6. 7. 8. 9.]
+x = np.arange(10.0)
+
+# y: [0. 0.15643447 0.30901699 0.4539905  0.58778525 0.70710678
+# 0.80901699 0.89100652 0.95105652 0.98768834]
+y = np.sin(np.arange(10.0) / 20.0 * np.pi)
+
+plt.errorbar(x, y, yerr=0.1)
+
+y = np.sin(np.arange(10.0) / 20.0 * np.pi) + 1
+plt.errorbar(x, y, yerr=0.1, uplims=True)
+
+y = np.sin(np.arange(10.0) / 20.0 * np.pi) + 2
+# upperlimits: [1 0 1 0 1 0 1 0 1 0]
+upperlimits = np.array([1, 0] * 5)
+# lowerlimits = [0 1 0 1 0 1 0 1 0 1]
+lowerlimits = np.array([0, 1] * 5)
+plt.errorbar(x, y, yerr=0.1, uplims=upperlimits, lolims=lowerlimits)
+
+plt.xlim(-1, 10)
+
+fig = plt.figure(1)
+x = np.arange(10.0) / 10.0
+y = (x + 0.1)**2
+
+plt.errorbar(x, y, xerr=0.1, xuplims=upperlimits, xlolims=lowerlimits)
+
+y = (x + 0.1)**3
+plt.errorbar(x + 0.6, y, xerr=0.1, xuplims=upperlimits, xlolims=lowerlimits)
+
+y = (x + 0.1)**4
+plt.errorbar(x + 1.2, y, xerr=0.1, xuplims=True)
+
+plt.xlim(-0.2, 2.4)
+plt.ylim(-0.1, 1.3)
+
+plt.show()
--- xxcs/DataVisualization/test.py
+++ xxcs/DataVisualization/test.py
@@ -9,11 +9,17 @@ import matplotlib.pyplot as plt
 # fig = plt.figure()
 
 
+# 图像对象
+fig = plt.figure(num='xxcs', figsize=(6, 3), dpi=100, facecolor='#02FFFF', frameon=True, edgecolor='#FF11B9')
 
-fig = plt.figure(num='fig1', figsize=(6, 3), dpi=100, facecolor='#1913FF', edgecolor='#FF11B9')
-print fig
+# 给图像对象添加主题
 # text_object: Text(0.5,0.98,'\xe8\xbf\x99\xe6\x98\xaf\xe4\xb8\xbb\xe9\xa2\x98')
-text_object = fig.suptitle("This is topic")
+text_object = fig.suptitle("My topic", x='0.9', y='1.0')
+
+# 关闭某个figure
+# None
+plt.close('xxcs')
+
 
 """
 # fig: Figure(640x480)
@@ -23,6 +29,6 @@ text_object = fig.suptitle("This is topic")
      [<matplotlib.axes._subplots.AxesSubplot object at 0x08649810>
       <matplotlib.axes._subplots.AxesSubplot object at 0x086669D0>]]
 """
-fig, ax_lst = plt.subplots(2, 2)
+fig1, ax_lst = plt.subplots(2, 2)
 
 plt.show()
